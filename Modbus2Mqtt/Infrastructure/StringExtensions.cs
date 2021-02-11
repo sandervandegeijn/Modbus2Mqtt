912f8dd46669521a624fc6ac5c96ea87aff1b1af
neographikal <sandervandegeijn@icloud.com>
2021-02-11 21:31:09 +0100

    Handy

--- Modbus2Mqtt/Infrastructure/StringExtensions.cs
+++ Modbus2Mqtt/Infrastructure/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace Modbus2Mqtt.Infrastructure
 {
@@ -7,6 +8,12 @@ namespace Modbus2Mqtt.Infrastructure
         public static string StripNonAlphaNumeric(this string str)
         {
             return Regex.Replace(str, @"[^a-zA-Z0-9]+", "-");
+        }
+        
+        public static T GetTfromString<T>(this string mystring)
+        {
+            var foo = TypeDescriptor.GetConverter(typeof(T));
+            return (T)(foo.ConvertFromInvariantString(mystring));
         }
     }
 }
