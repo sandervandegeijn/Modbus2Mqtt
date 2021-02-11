6f8f1a9c2206c38172fcc4d3f7717b1927d45aec
neographikal <sandervandegeijn@icloud.com>
2021-02-11 23:14:07 +0100

    Implemented proxy

--- Modbus2Mqtt/BackgroundServices/ModbusRequestQueueBackgroundService.cs
+++ Modbus2Mqtt/BackgroundServices/ModbusRequestQueueBackgroundService.cs
@@ -16,15 +16,13 @@ namespace Modbus2Mqtt.BackgroundServices
 {
     public class ModbusRequestQueueBackgroundService : BackgroundService
     {
-        private readonly ModbusClient _modbusClient;
-        private readonly IMediator _mediator;
+        private readonly ModbusRequestProxy _modbusRequestProxy;
         private readonly ILogger<ModbusRequestQueueBackgroundService> _logger;
         private static ConcurrentQueue<ModbusRequest> _queue;
 
-        public ModbusRequestQueueBackgroundService(ModbusClient modbusClient, IMediator mediator, ILogger<ModbusRequestQueueBackgroundService> logger)
+        public ModbusRequestQueueBackgroundService(ModbusRequestProxy modbusRequestProxy,ILogger<ModbusRequestQueueBackgroundService> logger)
         {
-            _modbusClient = modbusClient;
-            _mediator = mediator;
+            _modbusRequestProxy = modbusRequestProxy;
             _logger = logger;
             _queue = new ConcurrentQueue<ModbusRequest>();
         }
@@ -49,55 +47,7 @@ namespace Modbus2Mqtt.BackgroundServices
             {
                 _logger.LogDebug("Items in queue:" + _queue.Count);
                 _queue.TryDequeue(out var modbusRequest);
-                if (modbusRequest != null)
-                {
-                    _modbusClient.UnitIdentifier = Convert.ToByte(modbusRequest.Slave.SlaveId);
-                    try
-                    {
-                        //Function code 1
-                        if (modbusRequest.Register.Function == EnumModbusFunction.read_coil)
-                        {
-                            var result = _modbusClient.ReadCoils(modbusRequest.Register.Start, modbusRequest.Register.Registers);
-                            await _mediator.Publish(new ModbusCoilResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
-                        }
-
-                        //Function code 2
-                        if (modbusRequest.Register.Function == EnumModbusFunction.read_discrete_input)
-                        {
-                            var result = _modbusClient.ReadDiscreteInputs(modbusRequest.Register.Start, modbusRequest.Register.Registers);
-                            await _mediator.Publish(new ModbusCoilResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
-                        }
-
-                        //Function code 3
-                        if (modbusRequest.Register.Function == EnumModbusFunction.read_holding_registers)
-                        {
-                            var result = _modbusClient.ReadHoldingRegisters(modbusRequest.Register.Start, modbusRequest.Register.Registers);
-                            await _mediator.Publish(new ModbusRegisterResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
-                        }
-
-                        //Function code 4
-                        if (modbusRequest.Register.Function == EnumModbusFunction.read_input_registers)
-                        {
-                            var result = _modbusClient.ReadInputRegisters(modbusRequest.Register.Start, modbusRequest.Register.Registers);
-                            await _mediator.Publish(new ModbusRegisterResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
-                        }
-                        
-                        // //Function code 16
-                        // if (modbusRequest.Register.Function.ToLower().Equals("write_multiple_holding_registers"))
-                        // {
-                        //     HandleModbusWriteRequest(modbusRequest);
-                        // }
-                        
-                    }
-                    catch (CRCCheckFailedException)
-                    {
-                        _logger.LogError($"CRC exception for slave: {modbusRequest.Slave.Name} Register: {modbusRequest.Register.Name}");
-                    }
-                    catch (TimeoutException)
-                    {
-                        _logger.LogError($"Timeout for slave: {modbusRequest.Slave.Name} Register: {modbusRequest.Register.Name}");
-                    }
-                }
+                await _modbusRequestProxy.SendModbusRequest(modbusRequest, stoppingToken);
 
                 if (_queue.IsEmpty)
                 {
---
+++ Modbus2Mqtt/Infrastructure/Modbus/ModbusRequestProxy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EasyModbus;
+using EasyModbus.Exceptions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Modbus2Mqtt.Eventing.ModbusResult;
+using Modbus2Mqtt.Infrastructure.YmlConfiguration.DeviceDefinition;
+
+namespace Modbus2Mqtt.Infrastructure.Modbus
+{
+    public class ModbusRequestProxy
+    {
+        private readonly ModbusClient _modbusClient;
+        private readonly IMediator _mediator;
+        private readonly ILogger<ModbusRequestProxy> _logger;
+
+        public ModbusRequestProxy(ModbusClient modbusClient, IMediator mediator, ILogger<ModbusRequestProxy> logger)
+        {
+            _modbusClient = modbusClient;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task SendModbusRequest(ModbusRequest modbusRequest, CancellationToken stoppingToken)
+        {
+            if (modbusRequest != null)
+            {
+                _modbusClient.UnitIdentifier = Convert.ToByte(modbusRequest.Slave.SlaveId);
+                try
+                {
+                    if (modbusRequest.Register.Function == EnumModbusFunction.read_coil)
+                    {
+                        var result = _modbusClient.ReadCoils(modbusRequest.Register.Start, modbusRequest.Register.Registers);
+                        await _mediator.Publish(new ModbusCoilResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
+                    }
+                    
+                    if (modbusRequest.Register.Function == EnumModbusFunction.read_discrete_input)
+                    {
+                        var result = _modbusClient.ReadDiscreteInputs(modbusRequest.Register.Start, modbusRequest.Register.Registers);
+                        await _mediator.Publish(new ModbusCoilResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
+                    }
+                    
+                    if (modbusRequest.Register.Function == EnumModbusFunction.read_holding_registers)
+                    {
+                        var result = _modbusClient.ReadHoldingRegisters(modbusRequest.Register.Start, modbusRequest.Register.Registers);
+                        await _mediator.Publish(new ModbusRegisterResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
+                    }
+                    
+                    if (modbusRequest.Register.Function == EnumModbusFunction.read_input_registers)
+                    {
+                        var result = _modbusClient.ReadInputRegisters(modbusRequest.Register.Start, modbusRequest.Register.Registers);
+                        await _mediator.Publish(new ModbusRegisterResultEvent {Result = result, Register = modbusRequest.Register, Slave = modbusRequest.Slave}, stoppingToken);
+                    }
+                        
+                    // //Function code 16
+                    // if (modbusRequest.Register.Function.ToLower().Equals("write_multiple_holding_registers"))
+                    // {
+                    //     HandleModbusWriteRequest(modbusRequest);
+                    // }
+                        
+                }
+                catch (CRCCheckFailedException)
+                {
+                    _logger.LogError($"CRC exception for slave: {modbusRequest.Slave.Name} Register: {modbusRequest.Register.Name}");
+                }
+                catch (TimeoutException)
+                {
+                    _logger.LogError($"Timeout for slave: {modbusRequest.Slave.Name} Register: {modbusRequest.Register.Name}");
+                }
+            }
+        }
+    }
+}
